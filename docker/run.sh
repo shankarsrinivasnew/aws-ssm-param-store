679b742a47109efc2ce5a2a8195d41f97976da55
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-30 17:39:59 +0530

    sdf

--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
 
 for var in $1; do
     var_name=$(echo $var | awk -F : '{print $1}')
-    parameter=$(echo $var | awk -F : '{print $2)')
+    parameter=$(echo $var | awk -F : '{print $2}')
     value=$(aws ssm get-parameter --name $parameter --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g')
     echo export $(var_name)=${value}
 done
