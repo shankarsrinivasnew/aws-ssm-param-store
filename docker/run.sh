33e7aa3243e5bb38e544d467418d9fd18d041528
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-04-30 17:24:06 +0530

    asd

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,8 @@
 # VARIABLE:PARAMTER,VARIBALE:PARAMTER
 
 for var in $1; do
-    var_name = $(echo $var | awk -F : '{print $1}')
-    parameter = $(echo $var | awk -F : '{print $2)')
-    value = $(aws ssm get-parameter --name $parameter --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g')
-    echo export $(var_name) = ${value}
+    var_name=$(echo $var | awk -F : '{print $1}')
+    parameter=$(echo $var | awk -F : '{print $2)')
+    value=$(aws ssm get-parameter --name $parameter --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g')
+    echo export $(var_name)=${value}
 done
